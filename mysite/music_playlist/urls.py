ae2b7747ceaa559c273eb248765826654f275dfe
Abhishek Bopanna <abhishek.bopanna@gmail.com>
2014-10-29 02:27:20 +0530

    Added the add song function which as of now just adds the song to the database, it requires the media url

--- mysite/music_playlist/urls.py
+++ mysite/music_playlist/urls.py
@@ -4,4 +4,5 @@ from music_playlist import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^add_song/$', views.add_song, name='add_song'),
                        )
--- mysite/music_playlist/views.py
+++ mysite/music_playlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from music_playlist.models import Song
 
 # Create your views here.
@@ -7,6 +8,15 @@ def index(request):
     song_list = Song.objects.all()[:5]
     context = {'song_list' : song_list}
     return render(request, 'music_playlist/index.html', context)
+
+def add_song(request):
+    song = request.POST['add_song']
+    # TODO : Parse data before adding it to database
+    S = Song(media_url=song, song_url=song)
+    S.save()
+    return HttpResponseRedirect(reverse('music_playlist:index'))
+
+
 
 
 
